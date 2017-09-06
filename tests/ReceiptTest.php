f3d21e066aac355ad34a6bfdf6c8790fec71c8ba
Daisuke-sama <otaku.daisuke@gmail.com>
2017-09-06 20:28:45 +0300

    Created stub methods.

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -35,35 +35,66 @@ class ReceiptTest extends TestCase
 
     public function testTotal()
     {
-        $input = [0,2,5,8];
+        $input  = [0, 2, 5, 8];
         $coupon = null;    // dummy object
 
         $output = $this->receipt->total($input, null);
 
-        $this->assertEquals(15, $output, 'When summing the total should equal 15');
+        $this->assertEquals(
+            15, $output, 'When summing the total should equal 15'
+        );
     }
 
     /**
-     * Within the function is a "dummy" object placed. It is a coupon that exist, but doesn't affect.
+     * Within the function is a "dummy" object placed. It is a coupon that
+     * exist, but doesn't affect.
      */
     public function testTotalAndCoupon()
     {
-        $input = [0,2,5,8];
+        $input  = [0, 2, 5, 8];
         $coupon = 0.20;     // dummy object
 
         $output = $this->receipt->total($input, $coupon);
 
-        $this->assertEquals(12, $output, 'When summing the total should equal 12');
+        $this->assertEquals(
+            12, $output, 'When summing the total should equal 12'
+        );
     }
 
-    public function testTax() {
-        $inputAmount = 10.00;
-        $inputTax = 0.10;
+    public function testTax()
+    {
+        $inputAmount  = 10.00;
+        $inputTax     = 0.10;
         $outputAnswer = $this->receipt->tax($inputAmount, $inputTax);
         $this->assertEquals(
             1.00,
             $outputAnswer,
-            'Tax shoulf be equal 1'
+            'Tax should be equal 1.'
         );
+    }
+
+    /**
+     * Here is a stub creation for replacing of methods in the class, thereby
+     * excluding needed their real existing in the class, because we have written
+     * returns that we want to receive for our testing method passed its test.
+     */
+    public function testPostTaxTotal()
+    {
+        // creating a stub
+        // In other words, the two methods can absence in the testing class,
+        // and anyway test will pass.
+        $receipt = $this->getMockBuilder('TDD\\Receipt')
+            ->setMethods(['tax', 'total'])
+            ->getMock();
+        $receipt->method('total')
+            ->will($this->returnValue(10.00));
+        $receipt->method('tax')
+            ->will($this->returnValue(1.00));
+
+        // If the methods are used inside that tested function then its results
+        // will be replaced on created by mock above, i.e. 10.00 and 1.00.
+        $result = $receipt->postTaxTotal([1, 2, 5, 8], 0.20, null);
+
+        $this->assertEquals(11.00, $result);
     }
 }
